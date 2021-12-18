afe97864894b562a6ae45c63998457e721bf5e00
Magnus Karlsen <10944333+mamk95@users.noreply.github.com>
2021-12-18 14:33:21 +0100

    Cypress API test: Project

---
+++ tests/cypress/cypress/tests/api/project.spec.js
@@ -0,0 +1,50 @@
+import { createProject } from '../../support/project';
+
+describe('API', () => {
+
+    after(() => {
+        cy.deleteAllTestProjects();
+    });
+
+    it('responds with 404 on non-existent project', () => {
+        const nonexistentProjectId = 98935110;
+
+        cy.request({ url: `/api/project/${nonexistentProjectId}`, failOnStatusCode: false })
+            .should((response) => {
+                expect(response.status).to.eq(404);
+            });
+    });
+
+    it('can read new empty project', () => {
+        const projectName = `Cypress name ** ${new Date().getTime()}`;
+        const projectDesc = `Cypress desc ${new Date().getTime() + 1}`;
+        const projectPriority = 996999;
+        const projectHidden = Math.random() < 0.5; // random bool
+
+        cy.login();
+
+        createProject(projectName, projectDesc, projectPriority, projectHidden);
+
+        cy.visit('/admin/project');
+
+        cy.get('table')
+            .find('tbody tr')
+            .contains(projectName) // The project name table cell
+            .parent('tr') // The project row
+            .find('td')
+            .first() // The project ID is the first cell in the row
+            .as('projectId')
+            .then(function () {
+                cy.request(`/api/project/${this.projectId.text()}`)
+                    .should((response) => {
+                        expect(response.status).to.eq(200);
+                        expect(response.body.id).to.eq(+this.projectId.text());
+                        expect(response.body.name).to.eq(projectName);
+                        expect(response.body.description).to.eq(projectDesc);
+                        expect(response.body.sortOrder).to.eq(projectPriority);
+                        expect(response.body.hidden).to.eq(projectHidden);
+                        expect(response.body.releases).to.have.length(0);
+                    });
+            });
+    });
+});
