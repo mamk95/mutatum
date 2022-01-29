ceeea251311e498defcbe946033351a56c5efe7e
Magnus Karlsen <10944333+mamk95@users.noreply.github.com>
2022-01-29 13:34:35 +0100

    DB provider: MariaDB

---
+++ src/Changelog/Data/Database/MariaDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Changelog.Data.Database
+{
+    public class MariaDbContext : AppDbContext
+    {
+        public MariaDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
