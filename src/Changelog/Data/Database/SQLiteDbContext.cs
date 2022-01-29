c87139bc67026dceca18e1b9ea3eb52b189d5773
Magnus Karlsen <10944333+mamk95@users.noreply.github.com>
2022-01-29 13:33:30 +0100

    DB provider: SQLite

---
+++ src/Changelog/Data/Database/SQLiteDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Changelog.Data.Database
+{
+    public class SQLiteDbContext : AppDbContext
+    {
+        public SQLiteDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
---
+++ src/Changelog/Data/Options/Database/SQLiteOptions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Changelog.Data.Options.Database
+{
+    public class SQLiteOptions
+    {
+        /// <summary>
+        /// The connection string to connect to the SQLite database file.
+        /// <para/>
+        /// <example>
+        /// Example:
+        /// <code>
+        /// Filename=path/to/database/mutatum.sqlite
+        /// </code>
+        /// Example with Windows file path (remember to escape backslashes):
+        /// <code>
+        /// Filename=C:\\Users\\username\\Documents\\SQLite\\mutatum.sqlite
+        /// </code>
+        /// </example>
+        /// </summary>
+        public string ConnectionString { get; set; }
+    }
+}
