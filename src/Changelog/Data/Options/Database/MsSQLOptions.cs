30766e0fa57fe13ce91a619ca2f26fb8b432fb79
Magnus Karlsen <10944333+mamk95@users.noreply.github.com>
2022-01-29 13:34:17 +0100

    DB provider: Microsoft SQL

---
+++ src/Changelog/Data/Options/Database/MsSQLOptions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Changelog.Data.Options.Database
+{
+    public class MsSQLOptions
+    {
+        /// <summary>
+        /// The connection string to connect to the Microsoft SQL database server and database.
+        /// <para/>
+        /// <example>
+        /// Example:
+        /// <code>
+        /// Server=tcp:localhost,1433;Initial Catalog=databaseName;Persist Security Info=False;User ID=root;Password=1234;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;
+        /// </code>
+        /// </example>
+        /// </summary>
+        public string ConnectionString { get; set; }
+    }
+}
