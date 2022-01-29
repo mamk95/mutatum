2b230653dca593fea680b08a1c34475baa7e0dcc
Magnus Karlsen <10944333+mamk95@users.noreply.github.com>
2022-01-29 13:33:08 +0100

    DB provider: in-memory

---
+++ src/Changelog/Data/Options/Database/InMemoryOptions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Changelog.Data.Options.Database
+{
+    public class InMemoryOptions
+    {
+        /// <summary>
+        /// Totally optional name for the in-memory database. The name does
+        /// not matter at all for Mutatum's sake.
+        /// <para/>
+        /// <example>
+        /// Example:
+        /// <code>
+        /// Changelog
+        /// </code>
+        /// </example>
+        /// </summary>
+        public string DatabaseName { get; set; }
+    }
+}
