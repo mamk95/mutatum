b7d1a3deef843a0a30dc4239d3ffd8ab8bdd0e64
Magnus Karlsen <10944333+mamk95@users.noreply.github.com>
2022-01-29 13:34:51 +0100

    DB provider: Postgres

---
+++ src/Changelog/Data/Database/PostgresDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Changelog.Data.Database
+{
+    public class PostgresDbContext : AppDbContext
+    {
+        public PostgresDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
---
+++ src/Changelog/Data/Options/Database/PostgresOptions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Changelog.Data.Options.Database
+{
+    public class PostgresOptions
+    {
+        /// <summary>
+        /// The connection string to connect to the Postgres database server and database.
+        /// <para/>
+        /// <example>
+        /// Example:
+        /// <code>
+        /// Host=myserver;port=123;Username=mylogin;Password=mypass;Database=mydatabase;
+        /// </code>
+        /// </example>
+        /// </summary>
+        public string ConnectionString { get; set; }
+    }
+}
